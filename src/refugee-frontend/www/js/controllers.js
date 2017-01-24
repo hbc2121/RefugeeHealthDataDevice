a0ddbfec2c5c32d46835740d9abac192f05cbcf6
Brett Fouss <brettfouss@Bretts-MacBook-Pro.local>
2017-01-23 21:01:58 -0500

    Hooked up dummy data

--- src/refugee-frontend/www/js/controllers.js
+++ src/refugee-frontend/www/js/controllers.js
@@ -4,6 +4,10 @@ angular.module('starter.controllers', [])
 
 .controller('OverviewCtrl', function($scope) {})
 
-.controller('ExistingPatientCtrl', function($scope) {})
+.controller('ExistingPatientCtrl', function($scope, Questions) {
+        var questions = Questions.all().then(function (data) {
+                console.log(data.questions);
+        });
+})
 
 .controller('DoctorLoginCtrl', function($scope) {});
