4e5916e8ed6d2f5bc78d53128d8f4625be1b56e7
Danielle Zelin <daniellezelin@Danielles-MacBook-Air.local>
2017-01-23 23:54:48 -0500

    Design flow of app and each page contains basic design it will need to have

--- src/refugee-frontend/www/js/controllers.js
+++ src/refugee-frontend/www/js/controllers.js
@@ -4,17 +4,59 @@ angular.module('starter.controllers', [])
 
 .controller('OverviewCtrl', function($scope) {})
 
-.controller('ExistingPatientCtrl', function($scope, Questions) {
-        var questions = Questions.all().then(function (data) {
-                console.log(data.questions);
-        });
+.controller('VisitConfirmationCtrl', function($scope, $state) {
+
+    $scope.submit = function() {
+        $state.transitionTo('tab.overview');
+    }
+
 })
 
 .controller('DoctorLoginCtrl', function($scope, $state) {
 
     $scope.submit = function() {
-        console.log("trying to go somewhere");
         $state.transitionTo('tab.overview');
+    }
+
+})
+
+.controller('NewDoctorCtrl', function($scope, $state) {
+
+    $scope.submit = function() {
+        $state.transitionTo('doctor-login');
+    }
+
+})
+
+.controller('PatientLoginCtrl', function($scope, $state) {
+
+    $scope.submit = function() {
+        $state.transitionTo('existing-patient');
+    }
+
+})
+
+.controller('ExistingPatientCtrl', function($scope, Questions, $state) {
+    var questions = Questions.all().then(function (data) {
+        console.log(data.questions);
+    });
+    $scope.submit = function() {
+        $state.transitionTo('visit-confirmation');
+    }
+})
+
+.controller('NewPatientCtrl', function($scope, $state) {
+
+    $scope.submit = function() {
+        $state.transitionTo('new-patient-questions');
+    }
+
+})
+
+.controller('NewPatientQuestionsCtrl', function($scope, $state) {
+
+    $scope.submit = function() {
+        $state.transitionTo('visit-confirmation');
     }
 
 });
