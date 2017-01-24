63e7e0d815a68e7828c68c3334465b713bf0b864
Danielle Zelin <daniellezelin@Danielles-MacBook-Air.local>
2017-01-23 20:49:37 -0500

    Setup overview and patient login tabs

--- src/refugee-frontend/www/js/app.js
+++ src/refugee-frontend/www/js/app.js
@@ -32,54 +32,47 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
+  .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
 
   // Each tab has its own nav history stack:
+    .state('tab.overview', {
+      url: '/overview',
+      views: {
+        'tab-overview': {
+          templateUrl: 'templates/tab-overview.html',
+          controller: 'OverviewCtrl'
+        }
+      }
+    })
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.existing-patient', {
+    url: '/existing-patient',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-existing-patient': {
+        templateUrl: 'templates/tab-existing-patient.html',
+        controller: 'ExistingPatientCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
+  .state('doctor-login', {
+    url: '/doctor-login',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-doctor-login': {
+        templateUrl: 'templates/doctor-login.html',
+        controller: 'DoctorLoginCtrl'
       }
     }
   });
 
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/existing-patient');
 
 });
+
+
